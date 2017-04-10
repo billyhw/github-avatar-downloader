3b8525d2b4a2b3aa8fef64e8dfc9b4753db0b401
Billy Chang <billyheungwing@yahoo.ca>
2017-04-10 21:53:07 +0000

    getRepoContributors can now print all avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -15,16 +15,26 @@ var GITHUB_TOKEN = "5957ca0dfef761f8d00f1cd8a629f2da66b10f04";
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  // generate the request URL,
+  // generate the request URL, and set the User-Agent header
   // note that GITHUB_USER and GITHUB_TOKEN are defined globally.
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var requestURL = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
 
+  // run the request and use the callback to obtain results
+  request(requestURL, cb);
 }
 
 // a manual test
-
-getRepoContributors("jquery", "jquery", function (err, result) {
-  console.log("Errors", err);
-  console.log("Result:", result);
+getRepoContributors("jquery", "jquery", function (err, result, body) {
+  // return error if present
+  if (err) {
+    console.log("Errors", err);
+  }
+  // else print each avatar url
+  var json =  JSON.parse(body);
+  json.forEach(function(x) { console.log(x.avatar_url) ;});
 });
