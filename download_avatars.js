2e82b9e740733000efe64784fafe1954dda5157f
Billy Chang <billyheungwing@yahoo.ca>
2017-04-10 20:59:04 +0000

    added an empty getRepoContributors function into download_avatar.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,15 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  return request.get(`https://billyhw:5957ca0dfef761f8d00f1cd8a629f2da66b10f04/api.github.com/repos/${repoName}/${repoOwner}/contributors`);
+
+}
+
+getRepoContributors("jquery", "jquery", function (err, result) {
+  console.log("Errors", err);
+  console.log("Result:", result);
+});
